cfe536563f1e9b4304529afc3e0741042b3bc758
Semih Sipahi <semihsipahi@linux.com>
2021-08-01 01:27:03 +0300

    ISTE-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
+import PropTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.object.isRequired,
+  pageProps: PropTypes.object.isRequired
+};
 
 export default MyApp;
